cac4d449038486517ca6620901e5420997dd9146
Michael Villarruel <alejovillarruel@gmail.com>
2021-08-17 23:26:39 -0500

    useMovieDetails

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { Image, StyleSheet, Text, View, Dimensions, StatusBar } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/Navigation';
 import { ScrollView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useMovieDetails } from '../hooks/useMovieDetails';
 
 const screenHeight = Dimensions.get('screen').height;
 
@@ -12,19 +14,31 @@ export const DetailScreen = ({ route }: Props) => {
 
     const movie = route.params;
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-
+    const { isLoading, cast, movieFull } = useMovieDetails(movie.id);
+    console.log({movieFull})
     return (
         <ScrollView>
             <View style={styles.imageContainer}>
-                <Image
-                    source={{ uri }}
-                    style={styles.posterImage}
-                />
+                <View style={styles.imageBorder}>
+                    <Image
+                        source={{ uri }}
+                        style={styles.posterImage}
+                    />
+                </View>
             </View>
             <View style={styles.marginContainer}>
                 <Text style={styles.subTitle}>{movie.original_title}</Text>
                 <Text style={styles.title}>{movie.title}</Text>
+
             </View>
+            <View style={styles.marginContainer}>
+                <Icon
+                    name="star-outline"
+                    color='grey'
+                    size={20}
+                />
+            </View>
+
         </ScrollView>
     )
 }
@@ -32,7 +46,6 @@ export const DetailScreen = ({ route }: Props) => {
 const styles = StyleSheet.create({
     imageContainer: {
         width: '100%',
-        overflow: 'hidden',
         height: screenHeight * 0.7,
         shadowColor: "#000",
         shadowOffset: {
@@ -43,6 +56,12 @@ const styles = StyleSheet.create({
         shadowRadius: 6.27,
 
         elevation: 10,
+        borderBottomEndRadius: 25,
+        borderBottomStartRadius: 25,
+    },
+    imageBorder: {
+        flex: 1,
+        overflow: 'hidden',
         borderBottomEndRadius: 25,
         borderBottomStartRadius: 25,
     },
