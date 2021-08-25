88b1204172ea5a1280fa30e652483553825b664b
Michael Villarruel <alejovillarruel@gmail.com>
2021-08-25 00:07:50 -0500

    FadeIn and FadeOut, getColors and GradientContext

--- App.tsx
+++ App.tsx
@@ -3,16 +3,31 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
 import { StatusBar } from 'react-native';
+import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+const AppState = ({ children }: any) => {
+
+    return (
+        <GradientProvider>
+            {children}
+        </GradientProvider>
+    )
+
+}
 
 const App = () => {
     return (
         <NavigationContainer>
-            <StatusBar
-                barStyle='light-content'
-                backgroundColor='rgba(0,0,0, 0.2)'
-                translucent={true}
-            />
-            <Navigation />
+            <AppState>
+                <StatusBar
+                    barStyle='light-content'
+                    backgroundColor='rgba(0,0,0, 0.2)'
+                    translucent={true}
+                />
+                <Navigation />
+                {/* <FadeScreen /> */}
+            </AppState>
         </NavigationContainer>
     )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,6 +6,8 @@ import Carousel from 'react-native-snap-carousel';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
+import { GradientBackground } from '../components/GradientBackground';
+import { getImageColors } from '../helpers/getColors';
 
 const { width: windowWidth } = Dimensions.get('window');
 
@@ -13,6 +15,13 @@ export const HomeScreen = () => {
 
     const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
     const { top } = useSafeAreaInsets();
+
+    const getPosterColors = async (index: number) => {
+        const movie = nowPlaying[index];
+        const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        const [primary, secondary] = await getImageColors(uri);
+        console.log({primary,secondary})
+    }
 
     if (isLoading) {
         return (
@@ -27,24 +36,29 @@ export const HomeScreen = () => {
     }
 
     return (
-        <ScrollView>
+        <GradientBackground>
 
-            {/* Principal Carousel */}
-            <View style={{ height: 440, marginTop: top + 20 }}>
-                <Carousel
-                    data={nowPlaying}
-                    renderItem={({ item }: any) => <MoviePoster movie={item} />}
-                    sliderWidth={windowWidth}
-                    itemWidth={300}
-                    inactiveSlideOpacity={0.9}
-                />
-            </View>
+            <ScrollView>
 
-            {/* Popular Movies */}
-            <HorizontalSlider title="Popular" movies={popular} />
-            <HorizontalSlider title="Top Rated" movies={topRated} />
-            <HorizontalSlider title="Upcoming" movies={upcoming} />
+                {/* Principal Carousel */}
+                <View style={{ height: 440, marginTop: top + 20 }}>
+                    <Carousel
+                        data={nowPlaying}
+                        renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                        sliderWidth={windowWidth}
+                        itemWidth={300}
+                        inactiveSlideOpacity={0.9}
+                        onSnapToItem={index => getPosterColors(index)}
+                    />
+                </View>
 
-        </ScrollView>
+                {/* Popular Movies */}
+                <HorizontalSlider title="Popular" movies={popular} />
+                <HorizontalSlider title="Top Rated" movies={topRated} />
+                <HorizontalSlider title="Upcoming" movies={upcoming} />
+
+            </ScrollView>
+
+        </GradientBackground>
     )
 }
